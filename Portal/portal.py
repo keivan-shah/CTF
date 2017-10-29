7610ce4152678612185f2875b9bf92d76e53a53d
Deepkawa <kawadeep3@gmail.com>
2017-10-29 11:08:32 +0530

    Add files via upload

--- Portal/portal.py
+++ Portal/portal.py
@@ -10,17 +10,21 @@ answer = { 	'1':'answer1',
 			'3':'answer3',
 			'4':'answer4'}
 
+list = ['1', '2', '3', '4']
+
 @app.route('/')
 def index():
 	return "Please Go to '(website address)/(question_number)\'"
 
 @app.route('/<question>', methods=['GET', 'POST'])
 def check_answer(question):
-	if request.method == "POST":
-		if request.form.get("answer") == answer[question]:
-			return"Shout for the Volunteer"
-				
-	return render_template("check_answer.html",question=question)
+	if question in list:
+		if request.method == "POST":
+			if request.form.get("answer") == answer[question]:
+				return"Shout for the Volunteer"
+					
+		return render_template("check_answer.html",question=question)
+	return "Smart move but will not work here!!"
 
 if __name__ =="__main__":
 	app.run(debug=True)
