c3f0bf87b853e58a073ef9c961c2d9466803d401
keivan-shah <keivan-shah@users.noreply.github.com>
2017-11-11 14:11:06 +0530

    Added Final Zips

--- Portal/portal.py
+++ Portal/portal.py
@@ -2,10 +2,10 @@ from flask import Flask,render_template,flash,redirect,request,abort,send_from_d
 
 app = Flask(__name__)
 
-answer = { 	'1':'none',
-			'2':'accelerate',
-			'3':'esrever',
-			'4':'paint:rules',
+answer = {  '1':'none',
+            '2':'accelerate',
+            '3':'esrever',
+            '4':'paint:rules',
             '5':'Pablo Picasso',
             '6':'51f7fed12941cea5b15f66a92b92803b',
             '7':'KhuljaSimSim',
@@ -17,23 +17,25 @@ def index():
 
 @app.route('/download', methods=['GET', 'POST'])
 def download(): 
-    return send_from_directory(directory='static/download', filename="Questions.zip")
+    return send_from_directory(directory='static/download', filename="Questions.7z")
 
 @app.route('/<int:question>', methods=['GET', 'POST'])
 def check_answer(question):
-	if str(question) in answer:
-		if request.method == "POST":
-			if request.form.get("answer").lower() == answer[str(question)].lower():
-				return render_template("success.html", question=question)
-			else:
-				return render_template("error.html", question=question)
-	else:
-		abort(404)
-	return render_template("check_answer.html",question=question)
+    if str(question) in answer:
+        if request.method == "POST":
+            if request.form.get("answer").lower() == answer[str(question)].lower():
+                if(question==8):
+                    return render_template("final.html")
+                return render_template("success.html", question=question)
+            else:
+                return render_template("error.html", question=question)
+    else:
+        abort(404)
+    return render_template("check_answer.html",question=question)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ =="__main__":
-	app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
